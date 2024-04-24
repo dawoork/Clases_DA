2def1731203d625c15663583ee7959416b988939
Dawoork <lean.araque@gmail.com>
2024-04-24 20:05:40 -0300

    Actualizando_v1

---
+++ 4. Lenguaje SQL, funciones escalares y subconsultas/05_Micro04_P02_Distinct.sql
@@ -0,0 +1,3 @@
+-- PREGUNTA 1: Visualizar todos los valores distintos de la columna "ESTADO" en la tabla ESTADOS
+SELECT DISTINCT Estado 
+FROM Estados;
---
+++ 4. Lenguaje SQL, funciones escalares y subconsultas/06_Micro04_P03_Where.sql
@@ -0,0 +1,4 @@
+-- PREGUNTA 2: Seleccionar todas las columnas de 'Ciudades_USA' donde 'ID_ESTADO' sea igual a 5
+SELECT * 
+FROM Ciudades_USA 
+WHERE ID_Estado = 5;
